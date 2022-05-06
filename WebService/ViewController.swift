3664f36a73150544f64abd7fdfc5104c2a559351
Luis Fernando Cuevas Cuauhtle <luisfernandocuevas@outlook.com>
2022-05-05 19:26:36 -0500

    Error mixed

--- WebService/ViewController.swift
+++ WebService/ViewController.swift
@@ -141,6 +141,10 @@ class ViewController: UIViewController {
             self.searchItemsTableView.reloadData()
         }
     }
+    
+    func fixError(){
+        print("ERROR FIXED")
+    }
 
 }
 
